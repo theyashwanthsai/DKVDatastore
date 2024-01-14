f1abeb739a9a509a1336531b56fcdbcfc9dde466
Sai Yashwanth <taddishetty34@gmail.com>
2024-01-14 23:38:39 +0530

    Init: Added a basic roadmap

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+// There are multiple users, each users can have multiple clusters. Clusters is basically like an relation in mysql, collections in mysql (sort of, not exactly but kind of)
+
+// Create a struct data, this will contain the data in keyvalue form.
+
+
+func addData(){
+
+}
+
+func deleteData(){
+
+}
+
+func readData(){
+
+}
+
+func main(){
+
+}
+
+
